cf19adb35529036f144cc4c414f2d635ad7ab4e4
francyverg <37416456+francyverg@users.noreply.github.com>
2019-08-26 18:18:39 +0200

    create class.h
    iniziate classi Contatti e Schermata

---
+++ class.h
@@ -0,0 +1,18 @@
+class Contatti
+{
+	public:
+		char nome;
+		char cognome;
+		char numero;
+		void cerca();
+		void aggiungi();
+		void elimina();
+		
+}
+// l= lunghezza, h= altezza
+class Screen
+{
+	public:
+		char l;
+		char h;	
+}
