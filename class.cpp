92b94057cc3d454541027f6f5f018022caeab445
francyverg <37416456+francyverg@users.noreply.github.com>
2019-08-26 18:20:00 +0200

    create class.cpp
    iniziate le funzioni della classe Contatti

---
+++ class.cpp
@@ -0,0 +1,17 @@
+#include <iostream.h>
+include "classi.h"
+using namespace std;
+void Contatti::aggiungi()
+{
+	cout << "inserire nome: ";
+	cin >> nome;
+	cout <<"inserire cognome: ";
+	cin >> cognome;
+	cout << "inserire numero: "	;
+	cin >> numero;
+	cout <<endl;
+}
+void Contatti::elimina()
+{
+	
+}
